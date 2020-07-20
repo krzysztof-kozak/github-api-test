1b7450a013d21f94db28bdabc48d08d57ee08765
Krzysztof Kozak <hithard5@gmail.com>
2020-07-20 20:15:58 +0200

    added styling and js logic

---
+++ index.js
@@ -0,0 +1,25 @@
+import repos from './repos.js';
+const user = 'krzysztof-kozak';
+const API = `https://api.github.com/users/${user}/repos`;
+const list = document.querySelector('.list');
+
+/*--- copy json response and use locally to go around rate limiting ---*/
+
+/* 
+--------------------------------------
+
+fetch(API)
+  .then((response) => response.json())
+  .then((data) => console.log(data)); 
+  
+ -------------------------------------- 
+ */
+
+repos.forEach((repo) => {
+  const { name, html_url } = repo;
+
+  const listItem = document.createElement('li');
+  listItem.classList.add('list__item');
+  listItem.innerHTML = `${name}: <a href=${html_url}>${html_url}</a>`;
+  list.appendChild(listItem);
+});
